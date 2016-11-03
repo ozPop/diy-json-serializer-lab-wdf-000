2db53059ec5c8cea201f767ad260d6dedd42de38
ozPop <ozziepopovas@gmail.com>
2016-11-03 07:55:36 -0400

    implement show page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,21 +1,25 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [
+    :inventory, :description, :data, :show
+  ]
+
   def index
     @products = Product.all
   end
 
   def inventory
-    product = Product.find(params[:id])
-    render plain: product.inventory > 0 ? true : false
+    # product = Product.find(params[:id])
+    render plain: @product.inventory > 0 ? true : false
   end
 
   def description
-    product = Product.find(params[:id])
-    render plain: product.description
+    # product = Product.find(params[:id])
+    render plain: @product.description
   end
 
   def data
-    product = Product.find(params[:id])
-    render json: ProductSerializer.serialize(product)
+    # product = Product.find(params[:id])
+    render json: ProductSerializer.serialize(@product)
   end  
 
   def new
@@ -27,9 +31,16 @@ class ProductsController < ApplicationController
     redirect_to products_path
   end
 
+  def show
+  end
+
   private
 
   def product_params
     params.require(:product).permit(:name, :description, :inventory, :price)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
